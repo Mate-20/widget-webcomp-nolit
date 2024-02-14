120f294101461d572756947044498e6160e6abd2
Akash Jindal <20akash.jindal@gmail.com>
2024-02-14 15:50:00 +0530

    adding bundle.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Import all your JavaScript files here
+import './Card.js';
+import './MultipleData.js';
+import './SingleData.js';
+import './RegisterForm.js';
+import './Modal.js';
+import './EventGeni.js';
