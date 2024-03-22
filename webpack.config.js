c596e5b24a793e95fa9c4cca8718a525c6d54b28
Akash Jindal <20akash.jindal@gmail.com>
2024-03-22 11:07:11 +0530

    trying the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'eventgeni.js', // Output filename
   },
+  module: {
+    rules: [
+      {
+        test: /\.json$/, // Match JSON files
+        loader: 'json-loader', // Use json-loader to handle JSON files
+        type: 'javascript/auto', // Specify the type as 'javascript/auto' to avoid webpack 5 deprecation warnings
+      },
+    ],
+  },
 };
