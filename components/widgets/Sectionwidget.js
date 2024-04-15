cf1b1da8352a9adfc25b8b5ca1eca756bd3650db
Akash Jindal <20akash.jindal@gmail.com>
2024-04-15 16:58:13 +0530

    adding section widget endpoints

--- components/widgets/Sectionwidget.js
+++ components/widgets/Sectionwidget.js
@@ -2,7 +2,8 @@ class Sectionwidget extends HTMLElement{
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.fetchData();
+        this.fetchData()
+        this.data = null;
         this.toggleState = false; // this state is for opening the modal
         this.formData = {
             eventname: "demoevent",
@@ -11,21 +12,45 @@ class Sectionwidget extends HTMLElement{
             eventdate: "1/1/1",
             eventdescription: "demo description",
         };
+        this.sectionquery = ""
+        this.sectionid = ""
     }
 
     async fetchData() {
         try {
-            const response = await fetch("http://127.0.0.1:8080/configurationsSection.json");
-            this.data = await response.json();
+            const pageIdResponse = await fetch(`https://api.eventgeni.com/widgets/${this.sectionid}?type=page`);
+            const pageIdData = await pageIdResponse.json();
+            this.sectionquery = pageIdData.query;
+            const mainDataResponse = await fetch(`https://api.eventgeni.com/es/find?company=104&${this.sectionquery}`);
+            this.data = await mainDataResponse.json();
+            console.log("Data is this", this.data);
             this.render();
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
     connectedCallback() {
+        this.sectionid = this.getAttribute('section-id'); 
+        this.fetchData();
+        this.observeAttributes(); 
         this.addEventListener('modal-open', this.handleModalOpen.bind(this))
-        this.addEventListener('close-form', this.handleModalClose.bind(this))
+        // this.addEventListener('close-form', this.handleModalClose.bind(this))
         this.render()
+    }
+    observeAttributes() {
+        // Create a new MutationObserver instance
+        this.observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'section-id') {
+                    // When 'sticky-id' attribute changes, update the stickyid and fetch new data
+                    this.sectionid = mutation.target.getAttribute('section-id');
+                    console.log("widget app , section id is ", this.sectionid)
+                    this.fetchData();
+                }
+            });
+        });
+        // Observe changes to attributes
+        this.observer.observe(this, { attributes: true });
     }
     handleModalClose(){
         this.toggleState = false
